e22f0e6543e7bf0c9852a34494da1aac2575f495
watanabe_masahiro02 <watanabe_masahiro@5star.co.jp>
2022-10-21 17:17:35 +0900

    Itemの下準備

---
+++ example-app/webpack.mix.js
@@ -0,0 +1,13 @@
+let mix = require("laravel-mix");
+
+
+mix.js("resources/js/app.js", "public/js")
+    .vue()
+    .postCss("resources/css/app.css", "public/css")
+    .alias({
+        "@": "resources/js",
+    });
+
+if (mix.inProduction()) {
+    mix.version();
+}
